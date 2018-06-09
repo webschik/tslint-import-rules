e66a70f1f1e28dd7f957467489f714efb7474a07
webschik <webschik@gmail.com>
2018-06-09 12:27:57 +0200

    [feat] added test for tir-spaces-within-import rule

--- src/rules/tirSpacesWithinImportRule.ts
+++ src/rules/tirSpacesWithinImportRule.ts
@@ -1,7 +1,7 @@
 import * as ts from 'typescript';
 import * as Lint from 'tslint';
 
-const spaceSymbolPattern: RegExp = /[ ]/g;
+const nonSpaceSymbolPattern: RegExp = /\S/;
 
 export class Rule extends Lint.Rules.AbstractRule {
     apply (sourceFile: ts.SourceFile): Lint.RuleFailure[] {
@@ -10,38 +10,80 @@ export class Rule extends Lint.Rules.AbstractRule {
 }
 
 class RuleWalker extends Lint.RuleWalker {
+    private sourceText: string;
+
+    constructor (sourceFile: ts.SourceFile, options: Lint.IOptions) {
+        super(sourceFile, options);
+
+        this.sourceText = sourceFile.getText();
+    }
+
+    private addSpacesFailure (node: ts.ImportClause, count: number, beforeBracket?: boolean) {
+        const absCount: number = Math.abs(count);
+        const spacesText: string = `${ absCount } space${ absCount > 1 ? 's' : ''}`;
+        const positionText: string = beforeBracket ? `before '}'` : `after '{'`;
+
+        this.addFailureAtNode(
+            node,
+            `${ count > 0 ? 'Unexpected' : 'Expected' } ${ spacesText } ${ positionText }`
+        );
+    }
+
+    private testCharAndCollectSpaces (
+        node: ts.ImportClause,
+        expectedSpacesCount: number,
+        actualSpacesCount: number,
+        charIndex: number,
+        beforeBracket?: boolean
+    ): number|undefined {
+        const ch: string = this.sourceText[charIndex];
+
+        if (ch === '\n') {
+            if (actualSpacesCount) {
+                this.addSpacesFailure(node, actualSpacesCount, beforeBracket);
+            }
+
+            return;
+        }
+
+        if (nonSpaceSymbolPattern.test(ch)) {
+            const spacesDiff: number = actualSpacesCount - expectedSpacesCount;
+
+            if (spacesDiff !== 0) {
+                this.addSpacesFailure(node, spacesDiff, beforeBracket);
+            }
+
+            return;
+        }
+
+        return actualSpacesCount + 1;
+    }
+
     visitImportDeclaration (node: ts.ImportDeclaration) {
         const {importClause} = node;
 
         if (importClause) {
             const options = this.getOptions();
             const count: number = options[0] === 'never' ? 0 : Object.assign({count: 1}, options[1]).count;
+            const start: number = importClause.getStart();
+            const end: number = importClause.getEnd();
+            let spacesCount: number|undefined = 0;
 
+            for (let i = start + 1; i < end; i++) {
+                spacesCount = this.testCharAndCollectSpaces(importClause, count, spacesCount, i);
 
-
-            const {sourceText, sourceTextLength} = this;
-            let isAfterImportDeclaration: boolean = false;
-            let afterNewLineSymbolsCount: number = 0;
-
-            for (let i = node.getEnd(); i < sourceTextLength; i++) {
-                const ch: string = sourceText[i];
-
-                if (ch === '\n') {
-                    afterNewLineSymbolsCount++;
-                } else if (nonSpaceSymbolPattern.test(ch)) {
-                    isAfterImportDeclaration = sourceText.slice(i, i + 6) === 'import';
+                if (spacesCount === undefined) {
                     break;
                 }
             }
 
-            if (!isAfterImportDeclaration) {
-                if (always && (count !== afterNewLineSymbolsCount - 1)) {
-                    this.addFailureAtNode(
-                        node,
-                        `Expected ${ count } empty line${ count > 1 ? 's' : ''} after import declaration`
-                    );
-                } else if (!always && afterNewLineSymbolsCount > 1) {
-                    this.addFailureAtNode(node, 'Unexpected empty line after import declaration');
+            spacesCount = 0;
+
+            for (let i = end - 2; i > start; i--) {
+                spacesCount = this.testCharAndCollectSpaces(importClause, count, spacesCount, i, true);
+
+                if (spacesCount === undefined) {
+                    break;
                 }
             }
         }
